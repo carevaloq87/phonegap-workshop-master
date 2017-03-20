74527b7c6ebca3c8fd3360da592a2abb4e29d47d
Christian Arevalo <carevaloq87@gmail.com>
2017-03-20 17:52:20 +1100

    split home view from main JS file

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 var app = {
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -12,20 +7,12 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-
-    findByName: function() {
-        console.log('findByName');
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
+    
     initialize: function() {
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
     }
 
 };
