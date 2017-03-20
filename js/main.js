af6c8eaa4637f3a229c48897064f276e8b39e85d
Christian Arevalo <carevaloq87@gmail.com>
2017-03-20 17:27:03 +1100

    One page navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var app = {
 
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -29,4 +40,4 @@ var app = {
 };
 
 app.initialize();
-app.showAlert('Store Initialized', 'Info');
+app.renderHomeView();
